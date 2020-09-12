7fda543ddbd1dfb374a9070fade51a84a99aac2a
Sean DiGirolamo <s.digirolamo218@gmail.com>
2020-09-12 00:08:23 +0000

    asdasd

--- config.h
+++ config.h
@@ -93,7 +93,8 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 static const char *termcmd[]  = { "st", NULL };
 //static const char *browsercmd[] = {"tabbed", "surf", "-e", NULL };
 static const char *firefoxcmd[] = {"firefox", NULL};
-static const char *surfcmd[] = {"tabbed", "surf", "-e", NULL};
+//static const char *surfcmd[] = {"tabbed", "surf", "-e", NULL};
+static const char *vimbcmd[] = {"tabbed", "vimb", "-e", NULL};
 static const char *foobar2000cmd[] = {"foobar2000", "start", NULL };
 
 #include "movestack.c"
@@ -101,8 +102,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd} },
 	{ MODKEY,                       XK_s,      spawn,          {.v = termcmd} },
-	{ MODKEY,                       XK_f,      spawn,          {.v = surfcmd} },
-	{ MODKEY|ShiftMask,             XK_f,      spawn,          {.v = firefoxcmd} },
+	{ MODKEY,                       XK_f,      spawn,          {.v = firefoxcmd} },
 	{ MODKEY,                       XK_a,      spawn,          {.v = foobar2000cmd} },
 	//{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
