6a7da9190f178dd180b5dfa75fb9489ffc5ef5b5
Sean DiGirolamo <s.digirolamo218@gmail.com>
2021-06-07 12:40:32 -0400

    User can no longer switch focus while a client is fullscreen

--- dwm.c
+++ dwm.c
@@ -1102,26 +1102,28 @@ expose(XEvent *e)
 void
 focus(Client *c)
 {
-	if (!c || !ISVISIBLE(c))
-		for (c = selmon->stack; c && !ISVISIBLE(c); c = c->snext);
-	if (selmon->sel && selmon->sel != c)
-		unfocus(selmon->sel, 0);
-	if (c) {
-		if (c->mon != selmon)
-			selmon = c->mon;
-		if (c->isurgent)
-			seturgent(c, 0);
-		detachstack(c);
-		attachstack(c);
-		grabbuttons(c, 1);
-		XSetWindowBorder(dpy, c->win, scheme[SchemeSel][ColBorder].pixel);
-		setfocus(c);
-	} else {
-		XSetInputFocus(dpy, root, RevertToPointerRoot, CurrentTime);
-		XDeleteProperty(dpy, root, netatom[NetActiveWindow]);
+	if (!selmon->sel->isfullscreen) {
+		if (!c || !ISVISIBLE(c))
+			for (c = selmon->stack; c && !ISVISIBLE(c); c = c->snext);
+		if (selmon->sel && selmon->sel != c)
+			unfocus(selmon->sel, 0);
+		if (c) {
+			if (c->mon != selmon)
+				selmon = c->mon;
+			if (c->isurgent)
+				seturgent(c, 0);
+			detachstack(c);
+			attachstack(c);
+			grabbuttons(c, 1);
+			XSetWindowBorder(dpy, c->win, scheme[SchemeSel][ColBorder].pixel);
+			setfocus(c);
+		} else {
+			XSetInputFocus(dpy, root, RevertToPointerRoot, CurrentTime);
+			XDeleteProperty(dpy, root, netatom[NetActiveWindow]);
+		}
+		selmon->sel = c;
+		drawbars();
 	}
-	selmon->sel = c;
-	drawbars();
 }
 
 /* there are some broken focus acquiring clients needing extra handling */
