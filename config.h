0f3d74c6b803d8de43bb9498c1f12507470b553f
Sean DiGirolamo <s.digirolamo218@gmail.com>
2021-07-15 12:47:53 -0400

    Updated firefox-quick_open binding to website-quick_open

--- config.h
+++ config.h
@@ -107,7 +107,7 @@ ResourcePref resources[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
-static const char *ffqlcmd[] = { "firefox-quick_open", NULL };
+static const char *websitecmd[] = { "website-quick_open", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *termfloatcmd[] = { "st", "-c", "StFloat", NULL };
 //static const char *browsercmd[] = {"tabbed", "surf", "-e", NULL };
@@ -124,7 +124,7 @@ static const char *layoutmenu_cmd = "layoutmenu.sh";
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd} },
-	{ MODKEY|ShiftMask,             XK_d,      spawn,          {.v = ffqlcmd} },
+	{ MODKEY|ShiftMask,             XK_d,      spawn,          {.v = websitecmd} },
 	{ MODKEY,                       XK_s,      spawn,          {.v = termcmd} },
 	{ MODKEY|ShiftMask,             XK_s,      spawn,          {.v = termfloatcmd} },
 	{ MODKEY,                       XK_f,      spawn,          {.v = firefoxcmd} },
